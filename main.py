079341a4436eae47c4ef0deffc013b7428deeefa
XxTommyC11xX <119441199+XxTommyC11xX@users.noreply.github.com>
2023-03-30 20:11:02 +0100

    general game logic

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import pygame
+from sys import exit as sysexit
 
+pygame.init()
+
+WIDTH, HEIGHT = 800, 800
+
+screen = pygame.display.set_mode(WIDTH, HEIGHT)
+pygame.display.set_caption("Collaborative project")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sysexit()
+
+
+
+    pygame.display.update()
+    clock.tick(60)
