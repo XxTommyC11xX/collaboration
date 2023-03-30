3eb2bd8bf414376ffe0053791c9d0ffeef62d4b8
XxTommyC11xX <119441199+XxTommyC11xX@users.noreply.github.com>
2023-03-30 21:33:37 +0100

    stuff

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import pygame, player
+import pygame, tools
+import player as player_module
 from sys import exit as sysexit
 
 pygame.init()
 
 WIDTH, HEIGHT = 800, 800
+CENTERX, CENTERY = WIDTH/2, HEIGHT/2
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Collaborative project")
 clock = pygame.time.Clock()
+
+player = player_module.Player((CENTERX, CENTERY))
 
 while True:
     for event in pygame.event.get():
@@ -15,7 +19,7 @@ while True:
             pygame.quit()
             sysexit()
 
-
+    player.update()
 
     pygame.display.update()
     clock.tick(60)
