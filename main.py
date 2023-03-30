4f96a49edcae3ffc5a66120ca3c2d5dda9cf54b5
XxTommyC11xX <119441199+XxTommyC11xX@users.noreply.github.com>
2023-03-30 21:07:06 +0100

    remove crap

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ while True:
             pygame.quit()
             sysexit()
 
-    # act like I added something new here
-    # woah another new thing
+
 
     pygame.display.update()
     clock.tick(60)
